44ed4a15de2a173d778b4ef93f2ce46d3c686fbd
SvayMonirath <129480621+SvayMonirath@users.noreply.github.com>
2025-12-24 22:24:41 +0700

    Fix(Room): fix show/hide button

--- Frontend/src/pages/room/room_utils.js
+++ Frontend/src/pages/room/room_utils.js
@@ -131,16 +131,18 @@ export async function fetchRoomInfo() {
 // HIDE SHOW ROOM CODE
 const hideShowCodeBtn = document.getElementById("hide/show-code");
 
-hideShowCodeBtn?.addEventListener('click', () => {
-    if (roomCode.classList.contains("blur-md")) {
-        roomCode.classList.remove("blur-md");
-        hideShowCodeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/><circle cx="12" cy="12" r="3"/></svg>`;
+hideShowCodeBtn?.addEventListener("click", () => {
+    const isBlurred = roomCode.classList.contains("blur-sm"); // matches your HTML
 
+    if (isBlurred) {
+        roomCode.classList.remove("blur-sm");
+        hideShowCodeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/><circle cx="12" cy="12" r="3"/></svg>`;
     } else {
-        roomCode.classList.add("blur-md");
-        hideShowCodeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-off-icon lucide-eye-off"><path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49"/><path d="M14.084 14.158a3 3 0 0 1-4.242-4.242"/><path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143"/><path d="m2 2 20 20"/></svg>`
+        roomCode.classList.add("blur-sm");
+        hideShowCodeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49"/><path d="M14.084 14.158a3 3 0 0 1-4.242-4.242"/><path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143"/><path d="m2 2 20 20"/></svg>`;
     }
 });
+
 
 // TODO[X]: SHOW SETTING BTN
 
